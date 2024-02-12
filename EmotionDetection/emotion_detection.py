d1bebf89a775101addb4551bab4024c16ac5fed1
argareg <argaregvs@proton.me>
2024-02-12 04:03:04 -0500

    py: incorporated error handling and implemented code complience. * EmotionDetection/emotion_detection.py: added error handling. * server.py: added error handling and code has been rated 10 of 10. --
    run static code analyse:
    $ python3.11 -m pip install pylint
    $ pylint server.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,13 +6,27 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)
-    formatted_response = json.loads(response.text)
-    predicted_emotions = formatted_response["emotionPredictions"][0]["emotion"]
+    if response.status_code == 400:
+        anger = None
+        disgust = None
+        fear = None
+        joy = None
+        sadness = None
+        dominant_emotion = None
+    else:
+        formatted_response = json.loads(response.text)
+        predicted_emotions = formatted_response["emotionPredictions"][0]["emotion"]
+        anger = predicted_emotions["anger"]
+        disgust = predicted_emotions["disgust"]
+        fear = predicted_emotions["fear"]
+        joy = predicted_emotions["joy"]
+        sadness = predicted_emotions["sadness"]
+        dominant_emotion = list(filter(lambda x: predicted_emotions[x] == max(predicted_emotions.values()), predicted_emotions))[0]
     return {
-        'anger': predicted_emotions["anger"],
-        'disgust': predicted_emotions["disgust"],
-        'fear': predicted_emotions["fear"],
-        'joy': predicted_emotions["joy"],
-        'sadness': predicted_emotions["sadness"],
-        'dominant_emotion': list(filter(lambda x: predicted_emotions[x] == max(predicted_emotions.values()), predicted_emotions))[0]
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness,
+        'dominant_emotion': dominant_emotion
     }
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+""" An emotion detection application using Watson NLP library endpoint """
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,12 +6,19 @@ app = Flask("Emotion Detection")
 
 @app.route("/emotionDetector")
 def sent_analyzer():
+    """ Gets argument string, calls emotion_detector method and return response """
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-    return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(response["anger"], response["disgust"], response["fear"], response["joy"], response["sadness"], response["dominant_emotion"])
+    if response["dominant_emotion"] is None:
+        return "Invalid input ! Try again."
+    return f"For the given statement, the system response is 'anger': {response['anger']}, " \
+           f"'disgust': {response['disgust']}, 'fear': {response['fear']}, " \
+           f"'joy': {response['joy']} and 'sadness': {response['sadness']}. " \
+           f"The dominant emotion is {response['dominant_emotion']}."
 
 @app.route("/")
 def render_index_page():
+    """ Prints UI form """
     return render_template('index.html')
 
 if __name__ == "__main__":
